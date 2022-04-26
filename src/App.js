fc971bd263af0ac53ade301c6cb1ebac708a155a
paperpatch <patchen21@gmail.com>
2022-04-26 13:52:03 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+// import js libraries
+
+// import css in order
+import './App.css';
+
+// import components
+
+
+function App() {
+  return (
+    <div>
+
+      <main>
+        <>
+
+        </>
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
